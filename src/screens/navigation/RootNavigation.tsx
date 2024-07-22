9a3b48b7e6a7c6389dcfd0bc5f022fc2255be550
Mustafa Azad <mustafaazad533@gmail.com>
2024-07-22 17:22:55 +0530

    feat: RootNavigation Initialized

---
+++ src/screens/navigation/RootNavigation.tsx
@@ -0,0 +1,28 @@
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+	createStackNavigator,
+	TransitionPresets,
+} from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthNavigation from "./AuthNavigation";
+import TabNavigation from "./TabNavigation";
+
+const Stack = createStackNavigator();
+
+const RootNavigation = () => {
+	const [session, setSession] = useState(true);
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				{session ? (
+					<Stack.Screen name="TabNavigation" component={TabNavigation} />
+				) : (
+					<Stack.Screen name="AuthNavigation" component={AuthNavigation} />
+				)}
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+
+export default RootNavigation;
